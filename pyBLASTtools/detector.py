Added some utils function and correct a bug in phase calculation

pyBLASTtools/detector.py
@@ -453,33 +453,33 @@ class kidsutils():
         self.I = I
         self.Q = Q
 
-        self.phase = self.KIDphase(self.I, self.Q)
+        self.phase = self.KIDphase()
         self.mag = self.KIDmag()
 
-    def rotatePhase(self, I, Q):
+    def rotatePhase(self):
 
         '''
         Rotate phase for a KID
         '''
 
-        X = I+1j*Q
-        phi_avg = np.arctan2(np.mean(Q),np.mean(I))
+        X = self.I+1j*self.Q
+        phi_avg = np.arctan2(np.mean(self.Q),np.mean(self.I))
+        print(phi_avg, np.degrees(phi_avg))
         E = X*np.exp(-1j*phi_avg)
-        I = E.real
-        Q = E.imag
+        print(E)
+        self.I_rot = E.real
+        self.Q_rot = E.imag
 
-        return I, Q
-
-    def KIDphase(self, I, Q):
+    def KIDphase(self):
 
         '''
         Compute the phase of a KID. This is proportional to power, in particular
         Power = Phase/Responsivity
         '''
 
-        phibar = np.arctan2(np.mean(Q),np.mean(I))
-        I_rot, Q_rot = self.rotatePhase(I, Q)
-        phi = np.arctan2(Q,I)
+        phibar = np.arctan2(np.mean(self.Q),np.mean(self.I))
+        self.rotatePhase()
+        phi = np.arctan2(self.Q_rot, self.I_rot)
 
         return phi-phibar
 
@@ -490,13 +490,3 @@ class kidsutils():
         '''
 
         return np.sqrt(self.I**2+self.Q**2)
-
-    def KIDdf(self, target_path):
-
-        '''
-        Compute df of a kid knowing the target sweep located at target_path        
-        '''
-
-        target = np.loadtxt(target_path)
-
-        self.df = func(self.I, self.Q, target)

pyBLASTtools/mapmaker.py
@@ -547,7 +547,10 @@ class mapmaking(object):
 
         kernel = Gaussian2DKernel(x_stddev=std)
 
-        convolved_map = convolve(map_value, kernel, nan_treatment='fill', fill_value=0)
+        mask = np.ones_like(map_value)
+        mask[np.isfinite(map_value)] = 0
+
+        convolved_map = convolve(map_value, kernel, mask=mask, boundary=None)
 
         convolved_map[convolved_map==0] = np.nan
 

pyBLASTtools/plot.py
@@ -6,7 +6,6 @@ import pyBLASTtools.mapmaker as mp
 
 def plot_map(map_value, projection, idxpixel, title=None, centroid=None, save=False, save_path=None):
 
-
     wcs_proj = mp.wcs_world(projection.wcs.ctype, projection.wcs.crpix, projection.wcs.cdelt, \
                             projection.wcs.crval, projection.wcs.radesys, projection.wcs.equinox)
 
@@ -33,7 +32,7 @@ def plot_map(map_value, projection, idxpixel, title=None, centroid=None, save=Fa
 
     if centroid is not None:
         ax.plot(centroid[0]-np.floor(np.amin(idxpixel[:,0])), \
-                centroid[1]-np.floor(np.amin(idxpixel[:,1])), 'x', c='red')
+                centroid[1]-np.floor(np.amin(idxpixel[:,1])), 'x', c='red', transform=ax.get_transform('pixel'))
 
     c1 = ax.coords[0]           
     c2 = ax.coords[1]

pyBLASTtools/timing.py
@@ -5,6 +5,8 @@ from itertools import compress
 from scipy import interpolate
 import sys
 
+from . import utils
+
 class Error(Exception):
     """Base class for exceptions in this module."""
     pass
@@ -527,13 +529,15 @@ class dirfile_interp():
             self.d_master.flush('ctime_master_built')
             self.d_roach.flush(roach_time_str)
                 
-    def interp(self, field_master, field_roach, direction='mtr', interpolation_type='linear'):
+    def interp(self, field_master, field_roach, fs_master=None,\
+               direction='mtr', interpolation_type='linear'):
 
         '''
         Method for loading the data from the dirfile and then calling the interpolation method
         List of variables:
         - field_master: a string with the field coming from master
         - field_roach: a string with the field 
+        - field_master_array: an array with the field from the master file
         - direction: the direction of the interpolation
                      - mtr: interpolating master to roach
                      - rtm: interpolating roach to master 
@@ -543,29 +547,47 @@ class dirfile_interp():
         #On master every field has a different number of sample per frame
         #The index on the time needs to be shifted to take into consideration the different spf
 
-        spf_ctime = self.d_master.spf('ctime_master_built')
-        spf_field = self.d_master.spf(field_master)
+        if isinstance(field_master, np.ndarray):
+            field_master_array = field_master
 
-        if spf_ctime != spf_field:
-            field_master_array = self.master_to_100(field_master, spf_field, spf_ctime)
-        
+            if fs_master is None:
+                fs_master = int(len(field_master_array)/len(self.time_master)*self.d_master.spf('ctime_master_built'))
+
+            if len(field_master_array) != len(self.time_master):
+
+                time_master_new = utils.change_sampling_rate(self.time_master, field_master_array, \
+                                                             self.d_master.spf('ctime_master_built'), \
+                                                             fs_master)
         else:
-            field_master_array = self.d_master.getdata(field_master)
-            field_master_array = field_master_array[self.idx_start_master:self.idx_end_master]
+            spf_ctime = self.d_master.spf('ctime_master_built')
+            spf_field = self.d_master.spf(field_master)
+
+            if spf_ctime != spf_field:
+                field_master_array = self.master_to_100(field_master, spf_field, spf_ctime)
+            
+            else:
+                field_master_array = self.d_master.getdata(field_master)
+                field_master_array = field_master_array[self.idx_start_master:self.idx_end_master]
+
+            time_master_new = None
  
         field_roach_array = self.d_roach.getdata(field_roach, first_frame=self.idx_start_roach, \
                                                  num_frames=int(self.idx_end_roach-self.idx_start_roach))
 
         field_master_int, field_roach_int = self.interpolate(field_master_array, field_roach_array, \
+                                                             time_master_array=time_master_new,\
                                                              direction=direction,\
                                                              interpolation_type=interpolation_type)
 
-        self.d_master.flush(field_master)
+        try:
+            self.d_master.flush(field_master)
+        except TypeError:
+            pass 
         self.d_roach.flush(field_roach)
 
         return field_master_int, field_roach_int
 
-    def master_to_100(self, field_master, spf_field=None, spf_ctime=None):
+    def master_to_100(self, field_master, spf_field=None, spf_ctime=None, field_master_array=None):
 
         '''
         Function to convert master fields from a certain sampling frequency to 100 Hz.
@@ -578,28 +600,27 @@ class dirfile_interp():
         if spf_ctime is None:
             spf_ctime = self.d_master.spf('ctime_master_built')
 
-        idx_start_master_temp = int(np.floor(self.idx_start_master*spf_field/spf_ctime))
-        idx_end_master_temp = int(np.floor(self.idx_end_master*spf_field/spf_ctime))
-        
-        field_master_array = self.d_master.getdata(field_master)
-        field_master_array = field_master_array[idx_start_master_temp:idx_end_master_temp]
+        if field_master_array is None:
+            idx_start_master_temp = int(np.floor(self.idx_start_master*spf_field/spf_ctime))
+            idx_end_master_temp = int(np.floor(self.idx_end_master*spf_field/spf_ctime))
+            
+            field_master_array = self.d_master.getdata(field_master)
+            field_master_array = field_master_array[idx_start_master_temp:idx_end_master_temp]
+        else:
+            field_master_array = field_master_array
 
-        x_axis = np.arange(len(field_master_array))/(spf_field/spf_ctime)
+        return utils.change_sampling_rate(field_master_array, self.time_master, spf_field, spf_ctime)
 
-        f = interpolate.interp1d(x_axis, field_master_array, kind='linear', fill_value='extrapolate')
-        field_master_array = f(np.arange(len(self.time_master)))
-
-        assert len(field_master_array) == len(self.time_master)
-
-        return field_master_array
-
-    def interpolate(self, master_array, roach_array, direction='mtr', interpolation_type='linear'):
+    def interpolate(self, master_array, roach_array, time_master_array=None, \
+                    direction='mtr', interpolation_type='linear'):
 
         '''
         Method for interpolating the data.
         List of variables:
         - master_array: array with the data coming from master
         - roach_array: array with the data coming from roach
+        - time_master_array: an array with the time information for the master field. 
+                             If None, time_master=self.time_master
         - direction: the direction of the interpolation
                      - mtr: interpolating master to roach
                      - rtm: interpolating roach to master 
@@ -616,16 +637,22 @@ class dirfile_interp():
             print('The direction choosen for the interpolation is not correct. Choose between mtr and rtm')
             sys.exit(1)
 
+        if time_master_array is None:
+            time_master_array = self.time_master.copy()
+        else:
+            time_master_array = time_master_array
+
+
         if direction == 'rtm':
 
             f = interpolate.interp1d(self.time_roach, roach_array, kind=interpolation_type, \
                                      fill_value='extrapolate')
 
-            return master_array, f(self.time_master)
+            return master_array, f(time_master_array)
         
         elif direction == 'mtr':
 
-            f = interpolate.interp1d(self.time_master, master_array, kind=interpolation_type, \
+            f = interpolate.interp1d(time_master_array, master_array, kind=interpolation_type, \
                                      fill_value='extrapolate')
             
             return f(self.time_roach), roach_array