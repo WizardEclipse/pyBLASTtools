9acabc69255e057c966b27847b0b6dae40da3dd3
adrian <aksincla@asu.edu>
2020-05-04 08:20:44 -0700

    Changed det_test_v1.py script to include relative paths to data instead

--- test/det_test_v1.py
+++ test/det_test_v1.py
@@ -19,19 +19,23 @@ elif roach_num == 5:
     array = "250W"
     float_chans = 450
 
-flight_targ = np.load("flight_targ.npy")
+flight_targ = np.load("flight_targ_rel_paths.npy")
 flight_targ = flight_targ.astype("str")
-flight_chop_ts = np.load("flight_chop_ts.npy")
+flight_chop_ts = np.load("flight_ts_rel_paths.npy")
 flight_chop_ts = flight_chop_ts.astype("str")
 
+##################################3###############
+# Change hard drive path
+hard_drive_path = "/media/adrian/blast2020fc1/"
+##################################################
 det = pbt.detector.kidsutils()
 
 channel_number = 40
-# get timestreams for specific channel
-I_chan, Q_chan = det.getAllTs(flight_chop_ts[roach_num-1],roach_num,float_chans,start_samp = 32780000, stop_samp=32780000+4869)
+# get timestreams for specific channel, start_samp and stop_samp are roach indices.
+I_chan, Q_chan = det.getAllTs(hard_drive_path + flight_chop_ts[roach_num-1],roach_num,float_chans,start_samp = 32780000, stop_samp=32780000+4869)
 Z = I_chan + 1j*Q_chan
 # get target sweep for all channels
-s21_real, s21_imag = det.loadBinarySweepData(flight_targ[roach_num-1],vna=False)
+s21_real, s21_imag = det.loadBinarySweepData(hard_drive_path + flight_targ[roach_num-1],vna=False)
 s21_real_f, s21_imag_f = det.despike_targs(s21_real.T, s21_imag.T)
 s21_f = s21_real_f + 1j*s21_imag_f
 # get df's
